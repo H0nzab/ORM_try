dba25b00f65780e5c1a64a83b777968860173e06
H0nzab <honzab@hotmail.com>
2024-03-26 14:40:26 +0100

    working queries
    some basic queries, that work

--- index.js
+++ index.js
@@ -3,11 +3,55 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 async function main() {
-    const post = await prisma.post.update({
-      where: { id: 1 },
-      data: { published: true },
-    })
-    console.log(post)
+
+    //WRITE - create, createMany
+    /*const user = await prisma.user.create({
+        data: {
+          email: 'viola@prisma.io',
+          name: 'Viola',
+        },
+      })*/
+
+      /*const createMany = await prisma.user.createMany({
+        data: [
+          { name: 'Bob', email: 'bob@prisma.io' },
+          { name: 'Bobo', email: 'bob@prisma.io' }, // Duplicate unique key!
+          { name: 'Yewande', email: 'yewande@prisma.io' },
+          { name: 'Angelique', email: 'angelique@prisma.io' },
+        ],
+        skipDuplicates: true, // Skip 'Bobo'
+      })*/
+
+
+      //READ - finaUnique, findMany, findFirst
+      /*const user = await prisma.user.findMany({
+        where: {
+            email: {
+                endsWith: 'prisma.io',
+            },
+        },
+        select: {
+            name: true,
+            email: true,
+            posts: {
+                select: {
+                    likes: true,
+                },
+            },
+        },
+      })*/
+
+      // UPDATE - update, updateMany
+      /*const user = await prisma.user.update({
+        where:{
+            email: 'viola@prisma.io',
+        },
+        data: {
+            name: 'Viola de Magnificent',
+        },
+
+      })*/
+      console.log(user)
   }
 
 main()
